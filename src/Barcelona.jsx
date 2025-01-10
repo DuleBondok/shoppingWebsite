537394aa06a1de3f0b0a450242e4ce433a4bb9c1
DuleBondok <dulebondok@gmail.com>
2025-01-10 17:44:21 +0100

    changed img source

--- src/Barcelona.jsx
+++ src/Barcelona.jsx
@@ -45,10 +45,10 @@ function Barcelona() {
       <Header></Header>
       <div className="mainCorfuDiv">
         <div className="corfuImagesDiv">
-          <img className="corfuImg1" src="/public/barcelona1.jpg"></img>
+          <img className="corfuImg1" src="/barcelona1.jpg"></img>
           <div className="corfuImagesVerticalDiv">
-            <img className="corfuImg2" src="/public/barcelona2.jpg"></img>
-            <img className="corfuImg3" src="/public/barcelona3.jpg"></img>
+            <img className="corfuImg2" src="/barcelona2.jpg"></img>
+            <img className="corfuImg3" src="/barcelona3.jpg"></img>
           </div>
         </div>
         <div className="corfuHeaderDiv">
@@ -59,11 +59,11 @@ function Barcelona() {
         <div className="infoAboutDiv">
           <div className="corfuHorizontalDiv">
             <div className="corfuDateDiv">
-              <img src="/public/calendar.png" className="corfuCalender"></img>
+              <img src="/calendar.png" className="corfuCalender"></img>
               <h1 className="corfuNumberOfDays">4 days</h1>
             </div>
             <div className="corfuTravelDiv">
-              <img src="/public/airplane1.png" className="corfuBusImg"></img>
+              <img src="/airplane1.png" className="corfuBusImg"></img>
               <h1 className="corfuNumberOfDays">FLIGHT</h1>
             </div>
           </div>
